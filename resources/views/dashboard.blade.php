998ca7d1c7c2537e0f69ea8bc94d748ad3dbee3f
Patricia Lisboa <rlisboa.patricia@gmail.com>
2025-04-04 17:25:55 -0300

    route

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,16 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <div class="mb-4">
-                        <h3 class="text-lg font-medium text-gray-900">Welcome, {{ Auth::user()->name }}!</h3>
-                        <p class="mt-1 text-sm text-gray-600">You're logged in as {{ Auth::user()->email }}</p>
-                    </div>
-
-                    <div class="mt-6">
-                        <a href="{{ route('profile.edit') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                            Edit Profile
-                        </a>
-                    </div>
+                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
